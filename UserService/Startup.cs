07878ac49bebcba6a68a9ccd2296768bda10bc06
ngoyal0411 <ngoyal0411@gmail.com>
2020-04-11 20:14:12 +0530

    Integration of distributing logging using jaeger services.

--- UserService/Startup.cs
+++ UserService/Startup.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Jaeger;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using Jaeger.Senders;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +15,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OpenTracing;
+using OpenTracing.Util;
 using UserService.Repository;
 
 namespace UserService
@@ -27,8 +34,37 @@ namespace UserService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
             // configure DI for application services
             services.AddScoped<IUserBusiness, UserBusinessLogic>();
+            string serviceName1 = Assembly.GetEntryAssembly().GetName().Name;
+
+            services.AddSingleton<ITracer>(serviceProvider =>
+            {
+                string serviceName = Assembly.GetEntryAssembly().GetName().Name;
+
+                ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+                ISampler sampler = new ConstSampler(sample: true);
+
+                var reporter = new RemoteReporter.Builder()
+                        .WithLoggerFactory(loggerFactory)
+                        .WithSender(new UdpSender("jaeger-agent", 6831, 0))
+                        .Build();
+
+                ITracer tracer = new Tracer.Builder(serviceName)
+                    .WithLoggerFactory(loggerFactory)
+                    .WithSampler(sampler)
+                    .WithReporter(reporter)
+                    .Build();
+
+                GlobalTracer.Register(tracer);
+
+                return tracer;
+            });
+
+            services.AddOpenTracing();
+ 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
